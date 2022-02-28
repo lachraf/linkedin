ded7ef4546cbbc64e31f22ed29bcca856c4dd7a2
lachraf <achrafsallemsfaxiano@gmail.com>
2022-02-28 03:00:54 +0100

    linkedin

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import "./Components/NavBarr.css";
+import "./Components/Mainn.css"
+import NavBarr from "./Components/NavBarr";
+import Mainn from "./Components/Mainn";
+
+
+function App() {
+  return (
+    <div className="App" >
+      <NavBarr />
+      <Mainn />
+    </div>
+  );
+}
+
+export default App;
